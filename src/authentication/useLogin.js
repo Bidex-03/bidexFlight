699536d696de5238aadeecfde0d35667220dda1e
Bidex-03 <abdullahqaasim14@gmail.com>
2025-01-16 09:31:13 +0100

    refactor: remove commented console log in useLogin hook

--- src/authentication/useLogin.js
+++ src/authentication/useLogin.js
@@ -14,7 +14,6 @@ export function useLogin() {
         password,
       }),
     onSuccess: (user) => {
-      // console.log(user);
       queryClient.setQueryData(["user"], user.user);
       navigate("/home");
     },
